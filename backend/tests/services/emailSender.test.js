3b6e18215f9923e6d36cf2888145b288fce751a2
samuelmayna <samuelmayna@gmail.com>
2021-10-29 17:53:44 +0300

    Added tests for the email sender

---
+++ backend/tests/services/emailSender.test.js
@@ -0,0 +1,22 @@
+const { emailSender } = require("../../src/services");
+
+const MAX_TESTING_TIME_IN_MS = 20000;
+
+describe.skip("Email Sender", () => {
+  // the passing of this test is that it does not throw.
+  //ensure that the internet connection is stable and strong since the email
+  //sender has problem with slow internet connectivity.
+  it(
+    "should send email",
+    async () => {
+      const email = {
+        to: "samuelmayna@gmail.com",
+        subject: "Testing that emails can be sent",
+        html: `<p>Testing that an email can be sent from the application.</p>
+                       `,
+      };
+      await emailSender(email);
+    },
+    MAX_TESTING_TIME_IN_MS
+  );
+});
