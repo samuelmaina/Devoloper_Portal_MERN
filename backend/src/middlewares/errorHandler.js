7421dda1f9cc25d16a9d6b9e47495aabf806d635
samuelmayna <samuelmayna@gmail.com>
2022-01-26 22:37:48 +0300

    Added the error Handler for the whole appication

---
+++ backend/src/middlewares/errorHandler.js
@@ -0,0 +1,17 @@
+const { Responder } = require("../utils");
+
+module.exports = (app) => {
+  const notFound = (error, req, res, next) => {
+    try {
+      console.error(error);
+      return new Responder(res)
+        .withStatusCode(500)
+        .withMessage("Internal Server Error")
+        .send();
+    } catch (err) {
+      next(error);
+    }
+  };
+
+  app.use(notFound);
+};
