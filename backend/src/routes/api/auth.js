55c910d900b0440fa43adb827bf229d99ddbd8e0
samuelmayna <samuelmayna@gmail.com>
2022-01-26 21:58:33 +0300

    Added a route to verify email

--- backend/src/routes/api/auth.js
+++ backend/src/routes/api/auth.js
@@ -3,5 +3,6 @@ const { auth } = require("../../controllers");
 const router = require("express").Router();
 
 router.route("/sign-up/:type").post(auth.signUp);
+router.route("/verify/:token").get(auth.verifyEmail);
 
 module.exports = router;
