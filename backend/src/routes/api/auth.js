251cbe5a8293cc98cac7ed9bf372c9a7310fcdf2
samuelmayna <samuelmayna@gmail.com>
2022-01-27 11:57:45 +0300

    Added route for log-in

--- backend/src/routes/api/auth.js
+++ backend/src/routes/api/auth.js
@@ -1,10 +1,11 @@
 const { auth } = require("../../controllers");
 
-const { signUp, verifyEmail } = auth;
+const { signUp, verifyEmail, login } = auth;
 
 const router = require("express").Router();
 
 router.route("/sign-up/:type").post(signUp);
-router.route("/verify/:token").get(verifyEmail);
+router.route("/verify/:type/:token").get(verifyEmail);
+router.route("/log-in/:type/").post(login);
 
 module.exports = router;
