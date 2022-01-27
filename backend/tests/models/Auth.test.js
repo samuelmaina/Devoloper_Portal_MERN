0283ed4a4f4bd907a1fa12502b8af03860203a6f
samuelmayna <samuelmayna@gmail.com>
2022-01-27 10:16:46 +0300

    Add tests for findOneWithCredentials function in the models

--- backend/tests/models/Auth.test.js
+++ backend/tests/models/Auth.test.js
@@ -1,8 +1,11 @@
 const Auth = require("../../src/models/Auth");
-const { ensureEqual } = require("../utils/matchers");
+
+const { hash } = require("bcrypt");
+const { ensureEqual, ensureNull } = require("../utils/matchers");
 const { includeSetUpAndTearDowns, clearDb } = require("./utils");
 
-describe.skip("Auth Model", () => {
+const plain = "HashedPa55word?";
+describe("Auth Model", () => {
   includeSetUpAndTearDowns();
   afterEach(async () => {
     await clearDb();
@@ -19,31 +22,34 @@ describe.skip("Auth Model", () => {
     ensureDocHasTheRightData(doc, data);
   });
   it("findOneByEmail should return a doc with the given email", async () => {
-    const data1 = {
-      name: "Jonn Doe",
-      email: "test1@test.com",
-      password: "HashedPa55word?",
-      avatar: "/path/to/email/avatar",
-    };
-    const data2 = {
-      name: "Jonn Doe",
-      email: "test2@test.com",
-      password: "HashedPa55word?",
-      avatar: "/path/to/email/avatar",
-    };
-
-    const data3 = {
-      name: "Jonn Doe",
-      email: "test3@test.com",
-      password: "HashedPa55word?",
-      avatar: "/path/to/email/avatar",
-    };
-    await Auth.createOne(data1);
-    await Auth.createOne(data2);
-    await Auth.createOne(data3);
-
+    const data = await createTestDocs();
+    const data3 = data.data3;
     const found = await Auth.findOneByEmail(data3.email);
     ensureDocHasTheRightData(found, data3);
+  });
+
+  describe.only("findOneWithCredentials", () => {
+    let data;
+
+    beforeAll(async () => {
+      data = await createTestDocs();
+    });
+    it("should return doc when both email and password are okay", async () => {
+      const data3 = data.data3;
+      const { email } = data3;
+      const doc = await Auth.findOneWithCredentials(email, plain);
+      ensureDocHasTheRightData(doc, data3);
+    });
+    it("should return null when email is wrong", async () => {
+      const data3 = data.data3;
+      const doc = await Auth.findOneWithCredentials("some@email.com", plain);
+      ensureNull(doc);
+    });
+    it("should return null  when password is wrong", async () => {
+      const data3 = data.data3;
+      const doc = await Auth.findOneWithCredentials("some@email.com", plain);
+      ensureNull(doc);
+    });
   });
 });
 function ensureDocHasTheRightData(doc, expected) {
@@ -53,4 +59,30 @@ function ensureDocHasTheRightData(doc, expected) {
   //the password won't be hashed since It was hashed previously, hence they should be the same.
   ensureEqual(doc.password, expected.password);
   ensureEqual(doc.avatar, expected.avatar);
+}
+async function createTestDocs() {
+  const password = await hash(plain, 12);
+  const data1 = {
+    name: "Jonn Doe",
+    email: "test1@test.com",
+    password,
+    avatar: "/path/to/email/avatar",
+  };
+  const data2 = {
+    name: "Jonn Doe",
+    email: "test2@test.com",
+    password,
+    avatar: "/path/to/email/avatar",
+  };
+
+  const data3 = {
+    name: "Jonn Doe",
+    email: "test3@test.com",
+    password,
+    avatar: "/path/to/email/avatar",
+  };
+  await Auth.createOne(data1);
+  await Auth.createOne(data2);
+  await Auth.createOne(data3);
+  return { data1, data2, data3 };
 }
