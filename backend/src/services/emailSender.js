5b6a4e85f9ee646db0981fdf1c6aabd7b914c324
samuelmayna <samuelmayna@gmail.com>
2022-05-02 21:53:08 +0300

    Removed the logging

--- backend/src/services/emailSender.js
+++ backend/src/services/emailSender.js
@@ -4,14 +4,9 @@ sgMail.setApiKey(SENDGRID_API_KEY);
 
 module.exports = async (msg) => {
   try {
-    console.log("This is the email sender", EMAIL_SENDER);
     msg.from = EMAIL_SENDER;
-    const result = await sgMail.send(msg);
-    if (result) {
-      console.log("the email was sent succcessfully.");
-    }
+    await sgMail.send(msg);
   } catch (error) {
-    console.log(error);
     throw new Error(error);
   }
 };
