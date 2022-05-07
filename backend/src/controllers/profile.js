c98fb07fb8ad236760773f7471214c36cd5796e4
samuelmayna <samuelmayna@gmail.com>
2022-05-07 18:40:26 +0300

    Add code to render the profile if it exists

--- backend/src/controllers/profile.js
+++ backend/src/controllers/profile.js
@@ -10,6 +10,10 @@ exports.getUserProfile = async (req, res, next) => {
         .withStatusCode(404)
         .withMessage("You don't have any profile yet.Consider creating one.")
         .send();
+    return responder
+      .withStatusCode(200)
+      .attachDataToResBody(profile._doc)
+      .send();
   } catch (error) {
     next(error);
   }
@@ -19,10 +23,16 @@ exports.updateProfile = async (req, res, next) => {
   try {
     const responder = new Responder(res);
 
-    return responder
-      .withStatusCode(200)
-      .withMessage("Profile created successfully.")
-      .send();
+    const { body, user } = req;
+    body.user = user.id;
+    const profile = await Profile.findOneWithUserId(user.id);
+    if (!profile) {
+      await Profile.createOne(body);
+      return responder
+        .withStatusCode(200)
+        .withMessage("Profile created successfully.")
+        .send();
+    }
   } catch (error) {
     next(error);
   }
