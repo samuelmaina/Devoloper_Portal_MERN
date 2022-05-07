f48f4e074c509bc3d914381a2e9edcac7fa1bbb7
samuelmayna <samuelmayna@gmail.com>
2022-05-07 16:55:54 +0300

    Add Profile.js in the index

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -2,3 +2,4 @@ exports.UnVerified = require("./UnVerified");
 exports.TokenGenerator = require("./TokenGenerator");
 exports.User = require("./User");
 exports.Auth = require("./Auth");
+exports.Profile = require("./Profile");
