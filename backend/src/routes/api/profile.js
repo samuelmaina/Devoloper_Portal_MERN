3246fc6043012f714d7f2741971f031ff5b63361
samuelmayna <samuelmayna@gmail.com>
2022-05-07 16:50:19 +0300

    Add the updateProfle post path

--- backend/src/routes/api/profile.js
+++ backend/src/routes/api/profile.js
@@ -1,3 +1,12 @@
 const router = require("express").Router();
 
+const { profile } = require("../../controllers");
+const { auth } = require("../../middlewares/");
+
+const { ensureUserIsAuth } = auth;
+const { getUserProfile, updateProfile } = profile;
+
+router.route("/").get(ensureUserIsAuth, getUserProfile);
+router.route("/").post(ensureUserIsAuth, updateProfile);
+
 module.exports = router;
