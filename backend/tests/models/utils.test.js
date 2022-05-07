bfb79e108f8dcadfdde0e13e803453d3ce161ac0
samuelmayna <samuelmayna@gmail.com>
2022-05-07 15:58:40 +0300

    Added tests for the confirmPassword function

--- backend/tests/models/utils.test.js
+++ backend/tests/models/utils.test.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 const { MONGO_URI } = require("../../src/config");
-const { ensureEqual } = require("../utils/matchers");
+const { ensureEqual, ensureTruthy, ensureFalsy } = require("../utils/matchers");
 
-const { connectToDb, hashPassword } = require("../../src/models/utils");
+const {
+  connectToDb,
+  hashPassword,
+  confirmPassword,
+} = require("../../src/models/utils");
 
-describe.skip("Utils Methods", () => {
+describe("Utils Methods", () => {
   describe("connectTobb", () => {
     afterEach(async () => {
       await mongoose.connection.close();
@@ -15,9 +19,23 @@ describe.skip("Utils Methods", () => {
     });
   });
 
-  it(" hashPssword should hash password ", async () => {
+  it("hashPssword should hash password ", async () => {
     const example = "password5??";
     const hashed = await hashPassword(example);
     ensureEqual(hashed.length, 60);
+  });
+
+  describe("confirmPassword", () => {
+    it("should return true if the plain is a hash of the hash", async () => {
+      const example = "password5??";
+      const hashed = await hashPassword(example);
+      ensureTruthy(await confirmPassword(example, hashed));
+    });
+
+    it("should return false if the plain is not a hash of the hash", async () => {
+      const example = "password5??";
+      const hashed = await hashPassword(example);
+      ensureFalsy(await confirmPassword("randompassword", hashed));
+    });
   });
 });
