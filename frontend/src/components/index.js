e6da5b4d5166a481581da896763888e0d7411914
samuelmayna <samuelmayna@gmail.com>
2022-05-22 22:37:35 +0300

    index.js to unify require

---
+++ frontend/src/components/index.js
@@ -0,0 +1,9 @@
+export { default as Main } from "./Main";
+export { default as HomePage } from "./HomePage";
+export { default as NavBar } from "./NavBar";
+
+export { default as SignUp } from "./auth/SignUp";
+export { default as Login } from "./auth/Login";
+export { default as Reset } from "./auth/Reset";
+
+export { default as Routes } from "./Routes";
