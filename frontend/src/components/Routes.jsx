a4d840cb723b15fbc9cb8b441fbb824632447135
samuelmayna <samuelmayna@gmail.com>
2022-05-22 22:43:39 +0300

    Added the login and reset route

---
+++ frontend/src/components/Routes.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { Routes, Route } from "react-router-dom";
+
+import { HomePage, SignUp, Login, Reset } from "./index";
+
+const urls = [
+  { path: "/", element: HomePage },
+  { path: "/sign-up", element: SignUp },
+  { path: "/log-in", element: Login },
+  { path: "/reset", element: Reset },
+];
+
+function AppRoutes() {
+  return (
+    <Routes>
+      {urls.map((url) => (
+        <Route exact path={url.path} element={<url.element />}></Route>
+      ))}
+    </Routes>
+  );
+}
+
+export default AppRoutes;
