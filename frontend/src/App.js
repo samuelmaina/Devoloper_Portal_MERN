fbb7004a68d9bf4540e7065b15ffc4b35bbf2080
samuelmayna <samuelmayna@gmail.com>
2022-05-23 12:31:53 +0300

    Wrapped with redux provider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,18 @@ import React from "react";
 
 import { Main, NavBar } from "./components";
 
+import { Provider } from "react-redux";
+
+import store from "./redux/store";
+
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <NavBar />
-      <Main />;
-    </>
+      <Main />
+    </Provider>
   );
 }
 
