ca973bc52c2db463849cece0cf8f766868414c7c
samuelmayna <samuelmayna@gmail.com>
2022-05-23 12:31:15 +0300

    Added the redux store

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,15 @@
+import { applyMiddleware, createStore, compose } from "redux";
+
+import rootReducer from "./reducers";
+import thunk from "redux-thunk";
+
+const initialState = {};
+
+const middleware = [thunk];
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(applyMiddleware(...middleware))
+);
+
+export default store;
