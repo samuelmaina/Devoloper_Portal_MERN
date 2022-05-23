59f33d4f62158ade3c08b3b4025f160492b37457
samuelmayna <samuelmayna@gmail.com>
2022-05-23 16:56:38 +0300

    Added the email verifier

--- frontend/src/redux/actions/auth.js
+++ frontend/src/redux/actions/auth.js
@@ -1,20 +1,37 @@
 import axios from "axios";
 
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, GET_SUCCESS_MESSAGES } from "./types";
 
-export const registerUser = (data, history) => (dispatch) => {
-  console.log(data);
-  axios
-    .post("/api/auth/sign-up/user", data)
-    .then((res) => {
-      console.log(res);
-      return history.push("/log-in");
-    })
-    .catch((err) => {
-      console.log(err);
+export const registerUser = (data) => async (dispatch) => {
+  try {
+    const res = await axios.post("/api/auth/sign-up/user", data);
+    if (res) {
       dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data,
+        type: GET_SUCCESS_MESSAGES,
+        payload: res.data,
       });
+    }
+  } catch (err) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data,
     });
+  }
+};
+
+export const verifyEmail = (token) => async (dispatch) => {
+  try {
+    const res = await axios.get(`/api/auth/verify/${token}`);
+    if (res) {
+      dispatch({
+        type: GET_SUCCESS_MESSAGES,
+        payload: res.data,
+      });
+    }
+  } catch (err) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data,
+    });
+  }
 };
