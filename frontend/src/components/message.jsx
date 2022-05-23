9c9b432122d5279ae39d6f2f8af32cc89c817c07
samuelmayna <samuelmayna@gmail.com>
2022-05-23 15:14:45 +0300

    Tied the Message renders to clear up the Message and Info redux state. The message rendering and clean up go hand in hand

--- frontend/src/components/message.jsx
+++ frontend/src/components/message.jsx
@@ -1,5 +1,18 @@
+import { ERASE_MESSAGES } from "../redux/actions/types";
+import store from "../redux/store";
+
 const { message } = require("antd");
 
+function clearInfos() {
+  store.dispatch({ type: ERASE_MESSAGES });
+}
+
 export const renderErrorMessage = (error, duration) => {
-  return message.error(error, duration);
+  message.error(error, duration);
+  clearInfos();
+};
+
+export const renderSuccessMessage = (msg, duration) => {
+  message.success(msg, duration);
+  clearInfos();
 };
