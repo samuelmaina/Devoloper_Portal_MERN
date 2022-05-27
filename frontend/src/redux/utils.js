3b68eb93319eeac09a4e6014a840fb7ed3d2ae6e
samuelmayna <samuelmayna@gmail.com>
2022-05-27 14:37:46 +0300

    Removed the auth function from the utils directory to their own file in redux. They were not to be used globally

---
+++ frontend/src/redux/utils.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { SET_AUTH } from "./actions/types";
+import store from "./store";
+
+function setAuthTokenInHeader(token) {
+  if (token) {
+    axios.defaults.headers.common["Authorization"] = token;
+  } else {
+    delete axios.defaults.headers.common["Authorization"];
+  }
+}
+
+export function authenticate() {
+  const token = localStorage.getItem("jwt-token");
+  setAuthTokenInHeader(token);
+  store.dispatch({
+    type: SET_AUTH,
+    payload: token,
+  });
+}
+
+export function attachToken(token) {
+  localStorage.setItem("jwt-token", token);
+}
+export function removeToken() {
+  localStorage.removeItem("jwt-token");
+}
