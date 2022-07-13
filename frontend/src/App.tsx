39d7382bd865b65b238ea97cfc8b5b178a2c18c8
samuelmayna <samuelmayna@gmail.com>
2022-07-13 15:16:36 +0300

    Replaced the js frontend creaeted with react-create -app with the one containing typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import { Button } from 'antd';
+import './App.css';
+
+import 'antd/dist/antd.css';
+
+const App: FC = () => (
+  <div className="App">
+    <Button type="primary">Button</Button>
+  </div>
+);
+
+export default App;
--- frontend/src/config.ts
+++
@@ -1,2 +0,0 @@
-export const GEOLOCATION_KEY = process.env.REACT_APP_GEOLOCATION_KEY;
-export const TARTGET_URL = process.env.REACT_APP_TARTGET_URL;
