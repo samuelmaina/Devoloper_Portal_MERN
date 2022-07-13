5f2a1c6fa292b3f43eac767ca80ff362a1010961
samuelmayna <samuelmayna@gmail.com>
2022-07-13 23:14:07 +0300

    Added code to connect to the database

---
+++ backend/src/models/connectToDb.ts
@@ -0,0 +1,23 @@
+import { Sequelize } from "sequelize";
+
+import { DATABASE } from "../config";
+
+const connectToDb = () => {
+  try {
+    const user: string = "postgres";
+    const password: string = "postgres1234";
+    const port: number = 5432;
+    return new Sequelize(DATABASE, user, password, {
+      host: "localhost",
+      port,
+      dialect: "postgres",
+      logging: true,
+    });
+  } catch (error: any) {
+    //Throw so that the other parts don't continue to function if we are not
+    //connected to the database.
+    throw new Error(error);
+  }
+};
+
+export default connectToDb();
